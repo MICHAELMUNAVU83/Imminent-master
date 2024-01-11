c8f0bc1e397c887b1190f15fb31a7e6dd6b9a006
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2024-01-11 20:03:13 +0300

    added

--- js/main.js
+++ js/main.js
@@ -50,7 +50,7 @@ $(window).load(function () {
 /* END ------------------------------------------------------- */
 
 $("#countdown").countdown({
-  date: "Mar 01 2024",
+  date: "Feb 01 2024",
   render: function (data) {
     var el = $(this.el);
     el.empty()
